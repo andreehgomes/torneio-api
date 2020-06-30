b6f678e6a34057f82d347da15d5a575a035485df
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-06-30 15:43:14 -0300

    Cria método atualizar em associacaoService

--- src/main/java/com/torneioapi/service/AssociacaoService.java
+++ src/main/java/com/torneioapi/service/AssociacaoService.java
@@ -2,6 +2,7 @@ package com.torneioapi.service;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,13 @@ public class AssociacaoService {
 	
 	@Autowired
 	private AssociacaoRepository associacaoRepository;
+	
+	public Associacao atualizar(Long codigo, Associacao associacao) {
+		Associacao associacaoSalva = buscarAssociacaoPeloCodigo(codigo);
+		
+		BeanUtils.copyProperties(associacao, associacaoSalva, "id");
+		return associacaoRepository.save(associacaoSalva);
+	}
 	
 	private Associacao buscarAssociacaoPeloCodigo(Long codigo) {
 		Associacao associacaoSalva = associacaoRepository.findById(codigo).get();
