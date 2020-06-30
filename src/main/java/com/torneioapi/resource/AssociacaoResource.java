c183cbb50d01d85213f01ae60da33d8e492a3d2a
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-06-30 14:57:24 -0300

    Cria metodo findById()

--- src/main/java/com/torneioapi/resource/AssociacaoResource.java
+++ src/main/java/com/torneioapi/resource/AssociacaoResource.java
@@ -1,9 +1,12 @@
 package com.torneioapi.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,13 @@ public class AssociacaoResource {
 	@GetMapping
 	public List<Associacao> listar() {
 		return associacaoRepository.findAll();
+	}
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Associacao> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Associacao> retorno = associacaoRepository.findById(codigo);
+
+		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
 	}
 
 }
