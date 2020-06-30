3e39e19be77192cce75e98e7c9476ad949ce2d5b
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-06-30 15:19:57 -0300

    Cria metodo incluir nova associacao

--- src/main/java/com/torneioapi/resource/AssociacaoResource.java
+++ src/main/java/com/torneioapi/resource/AssociacaoResource.java
@@ -3,13 +3,21 @@ package com.torneioapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Associacao;
 import com.torneioapi.repository.AssociacaoRepository;
 
@@ -19,6 +27,9 @@ public class AssociacaoResource {
 
 	@Autowired
 	private AssociacaoRepository associacaoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Associacao> listar() {
@@ -30,6 +41,15 @@ public class AssociacaoResource {
 		Optional<Associacao> retorno = associacaoRepository.findById(codigo);
 
 		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Associacao> incluir (@Valid @RequestBody Associacao associacao, HttpServletResponse response){
+		Associacao associacaoSalva = associacaoRepository.save(associacao);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, associacaoSalva.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(associacaoSalva);
 	}
 
 }
