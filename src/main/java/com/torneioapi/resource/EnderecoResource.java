41fc3cb3d097974fa7a04b018b0a983f7f24b774
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-07-02 17:39:10 -0300

    Cria CRUD para a entidade endereco

--- src/main/java/com/torneioapi/resource/EnderecoResource.java
+++ src/main/java/com/torneioapi/resource/EnderecoResource.java
@@ -3,15 +3,25 @@ package com.torneioapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Endereco;
 import com.torneioapi.repository.EnderecoRepository;
+import com.torneioapi.service.EnderecoService;
 
 @RestController
 @RequestMapping("/enderecos")
@@ -19,6 +29,12 @@ public class EnderecoResource {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private EnderecoService enderecoService;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Endereco> listar(){
@@ -29,6 +45,21 @@ public class EnderecoResource {
 	public ResponseEntity<Endereco> buscarPeloCodigo(@PathVariable Long codigo){
 		Optional<Endereco> retorno = enderecoRepository.findById(codigo);
 		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Endereco> incluir(@Valid @RequestBody Endereco endereco, HttpServletResponse response){
+		Endereco enderecoSalvo = enderecoRepository.save(endereco);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, enderecoSalvo.getId()));
+		
+		return ResponseEntity.status(HttpStatus.OK).body(enderecoSalvo);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Endereco> atualizar(@PathVariable Long codigo, @Valid @RequestBody Endereco endereco){
+		Endereco enderecoSalvo = enderecoService.atualizar(codigo, endereco);
+		return ResponseEntity.status(HttpStatus.OK).body(enderecoSalvo);
 	}
 
 }
