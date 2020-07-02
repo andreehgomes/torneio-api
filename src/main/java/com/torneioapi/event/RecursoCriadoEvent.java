a2d83b8b2f0432ef3e4de147f0469c505a69cfff
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-07-01 22:37:10 -0300

    Modifica contrutor do RecursoCriadoEvent para aceitar id como String

--- src/main/java/com/torneioapi/event/RecursoCriadoEvent.java
+++ src/main/java/com/torneioapi/event/RecursoCriadoEvent.java
@@ -10,11 +10,18 @@ public class RecursoCriadoEvent extends ApplicationEvent {
 	
 	private HttpServletResponse response;
 	private Long codigo;
+	private String codigoText;
 
 	public RecursoCriadoEvent(Object source, HttpServletResponse response, Long codigo) {
 		super(source);
 		this.response = response;
 		this.codigo = codigo;		
+	}
+	
+	public RecursoCriadoEvent(Object source, HttpServletResponse response, String codigo) {
+		super(source);
+		this.response = response;
+		this.codigoText = codigo;	
 	}
 
 	public HttpServletResponse getResponse() {
@@ -23,6 +30,10 @@ public class RecursoCriadoEvent extends ApplicationEvent {
 
 	public Long getCodigo() {
 		return codigo;
+	}
+	
+	public String getCodigoText() {
+		return codigoText;
 	}
 	
 
