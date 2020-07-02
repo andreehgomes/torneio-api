a5ad69bea4991898dcb086dca473e75bde3baeaf
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-07-02 10:28:25 -0300

    Ajuste de relacionamento com entidade Ave

--- src/main/java/com/torneioapi/model/Criador.java
+++ src/main/java/com/torneioapi/model/Criador.java
@@ -1,11 +1,14 @@
 package com.torneioapi.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -31,6 +34,11 @@ public class Criador {
 	private Associacao associacao;
 	private Long fk_id_endereco;
 	private Timestamp data_cadastro;
+	
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "criador")
+	private List<Ave> aves;
+	
 	public String getCpf() {
 		return cpf;
 	}
