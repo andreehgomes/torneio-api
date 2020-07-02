1fc2b6361133485fe77831f917a36b139bb57578
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-07-02 10:28:56 -0300

    Ajuste de relacionamento com a entidade Ave

--- src/main/java/com/torneioapi/model/Especie.java
+++ src/main/java/com/torneioapi/model/Especie.java
@@ -1,13 +1,19 @@
 package com.torneioapi.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -24,6 +30,10 @@ public class Especie {
 	
 	@Column(name = "nomecientifico")
 	private String nomeCientifico;
+	
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "especie")
+	private List<Ave> aves;
 
 	public Long getId() {
 		return id;
