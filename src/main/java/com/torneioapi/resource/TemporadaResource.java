388f669eeb1613b929bd59fe3d29a344734fb493
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-19 19:15:23 -0300

    Cria método de insercão de nova temporada

--- src/main/java/com/torneioapi/resource/TemporadaResource.java
+++ src/main/java/com/torneioapi/resource/TemporadaResource.java
@@ -3,13 +3,21 @@ package com.torneioapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Temporada;
 import com.torneioapi.repository.TemporadaRepository;
 
@@ -19,6 +27,9 @@ public class TemporadaResource {
 
 	@Autowired
 	private TemporadaRepository temporadaRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Temporada> listar() {
@@ -31,5 +42,27 @@ public class TemporadaResource {
 		
 		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
 	}
+	
+	@PostMapping
+	public ResponseEntity<Temporada> incluir(@Valid @RequestBody Temporada temporada, HttpServletResponse response){
+		Temporada temporadaSalva = temporadaRepository.save(temporada);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, temporadaSalva.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(temporadaSalva);
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
