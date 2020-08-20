ae100e47d26a7811e3f990b5ab86b84d2a90a8b2
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-19 23:06:02 -0300

    Cria método Atualizar para Temporada

--- src/main/java/com/torneioapi/resource/TemporadaResource.java
+++ src/main/java/com/torneioapi/resource/TemporadaResource.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Temporada;
 import com.torneioapi.repository.TemporadaRepository;
+import com.torneioapi.service.TemporadaService;
 
 @RestController
 @RequestMapping("/temporadas")
@@ -30,6 +32,9 @@ public class TemporadaResource {
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@Autowired
+	private TemporadaService temporadaService;
 
 	@GetMapping
 	public List<Temporada> listar() {
@@ -50,6 +55,12 @@ public class TemporadaResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, temporadaSalva.getId()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(temporadaSalva);
+	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Temporada> atualizar(@PathVariable Long codigo, @Valid @RequestBody Temporada temporada){
+		Temporada temporadaSalva = temporadaService.atualizar(codigo, temporada);
+		return ResponseEntity.status(HttpStatus.OK).body(temporadaSalva);
 	}
 
 }
