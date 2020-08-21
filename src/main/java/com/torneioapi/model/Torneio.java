da9df02b6b74e1b1e8e98ff55a9c78c48d4ede5c
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-21 17:21:47 -0300

    Cria model para Torneio

---
+++ src/main/java/com/torneioapi/model/Torneio.java
@@ -0,0 +1,164 @@
+package com.torneioapi.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "torneio")
+public class Torneio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Max(250)
+	private String nome;
+	
+	@Max(500)
+	private String descricao;
+	
+	@NotNull
+	private Date datainicio;
+	private Date datafim;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name = "fk_id_temporada")
+	private Temporada temporada;
+	
+	@NotNull
+	@OneToOne
+	@JoinColumn(name = "fk_id_especie")
+	private Especie especie;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getDatainicio() {
+		return datainicio;
+	}
+
+	public void setDatainicio(Date datainicio) {
+		this.datainicio = datainicio;
+	}
+
+	public Date getDatafim() {
+		return datafim;
+	}
+
+	public void setDatafim(Date datafim) {
+		this.datafim = datafim;
+	}
+
+	public Temporada getTemporada() {
+		return temporada;
+	}
+
+	public void setTemporada(Temporada temporada) {
+		this.temporada = temporada;
+	}
+
+	public Especie getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(Especie especie) {
+		this.especie = especie;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datafim == null) ? 0 : datafim.hashCode());
+		result = prime * result + ((datainicio == null) ? 0 : datainicio.hashCode());
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + ((especie == null) ? 0 : especie.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((temporada == null) ? 0 : temporada.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Torneio other = (Torneio) obj;
+		if (datafim == null) {
+			if (other.datafim != null)
+				return false;
+		} else if (!datafim.equals(other.datafim))
+			return false;
+		if (datainicio == null) {
+			if (other.datainicio != null)
+				return false;
+		} else if (!datainicio.equals(other.datainicio))
+			return false;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (especie == null) {
+			if (other.especie != null)
+				return false;
+		} else if (!especie.equals(other.especie))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (temporada == null) {
+			if (other.temporada != null)
+				return false;
+		} else if (!temporada.equals(other.temporada))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
