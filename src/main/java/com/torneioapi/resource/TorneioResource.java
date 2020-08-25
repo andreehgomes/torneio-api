7690e3ac8a9d3e78e9c2ffa2f450e208c0ef845a
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-25 15:37:10 -0300

    Cria método incluir em TorneioResource

--- src/main/java/com/torneioapi/resource/TorneioResource.java
+++ src/main/java/com/torneioapi/resource/TorneioResource.java
@@ -3,14 +3,21 @@ package com.torneioapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Torneio;
 import com.torneioapi.repository.TorneioRepository;
 
@@ -21,6 +28,7 @@ public class TorneioResource {
 	@Autowired
 	private TorneioRepository torneioRepository;
 	
+	@Autowired
 	private ApplicationEventPublisher publisher;	
 	
 	@GetMapping
@@ -33,5 +41,14 @@ public class TorneioResource {
 		Optional<Torneio> retorno = torneioRepository.findById(codigo);
 		
 		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Torneio> incluir(@Valid @RequestBody Torneio torneio, HttpServletResponse response){
+		Torneio torneioSalvo = torneioRepository.save(torneio);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, torneioSalvo.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(torneioSalvo);
 	}
 }
