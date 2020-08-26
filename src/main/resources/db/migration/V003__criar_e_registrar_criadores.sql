cc5f078c1efd003eef242bed5419c2e28c17a2e0
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-26 11:20:15 -0300

    Limpa fk da migration V003

--- src/main/resources/db/migration/V003__criar_e_registrar_criadores.sql
+++ src/main/resources/db/migration/V003__criar_e_registrar_criadores.sql
@@ -4,13 +4,11 @@ CREATE TABLE IF NOT EXISTS criador (
 	nome VARCHAR(100),
 	sobrenome VARCHAR(150),
 	telefone VARCHAR(15),
-	adm BOOLEAN DEFAULT false,
-	fk_id_associacao BIGINT,
 	fk_id_endereco BIGINT,
 	data_cadastro TIMESTAMP NOT NULL DEFAULT 'now()'
 );
 
-INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone, fk_id_associacao) VALUES ('07011122241', '123451', 'Carlos Eduardo', 'Ovçar Gomes', '43999999999');
-INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone, fk_id_associacao) VALUES ('07011122242', '123452', 'Eduardo', 'Gomes', '43999999999', 1);
-INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone, fk_id_associacao) VALUES ('07011122243', '123453', 'André Felipe', 'Felício Gomes', '43999999999');
-INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone, fk_id_associacao) VALUES ('07011122244', '123454', 'Augusto Francisco', 'Felício Gomes', '43999999999');
+INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone) VALUES ('07011122241', '123451', 'Carlos Eduardo', 'Ovçar Gomes', '43999999999');
+INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone) VALUES ('07011122242', '123452', 'Eduardo', 'Gomes', '43999999999');
+INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone) VALUES ('07011122243', '123453', 'André Felipe', 'Felício Gomes', '43999999999');
+INSERT INTO criador(cpf, ibama, nome, sobrenome, telefone) VALUES ('07011122244', '123454', 'Augusto Francisco', 'Felício Gomes', '43999999999');
