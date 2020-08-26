e58c42b80f8a50ca60233452f6d766019f7886a5
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-08-26 13:29:58 -0300

    Cria getters e setters para AssociacaoCriador

--- src/main/java/com/torneioapi/model/AssociacaoCriador.java
+++ src/main/java/com/torneioapi/model/AssociacaoCriador.java
@@ -36,5 +36,104 @@ public class AssociacaoCriador {
 	@ManyToOne
 	@JoinColumn(name = "fk_id_criador")
 	private Criador criador;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public boolean isAdm() {
+		return adm;
+	}
+
+	public void setAdm(boolean adm) {
+		this.adm = adm;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public Associacao getAssociacao() {
+		return associacao;
+	}
+
+	public void setAssociacao(Associacao associacao) {
+		this.associacao = associacao;
+	}
+
+	public Criador getCriador() {
+		return criador;
+	}
+
+	public void setCriador(Criador criador) {
+		this.criador = criador;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (adm ? 1231 : 1237);
+		result = prime * result + ((associacao == null) ? 0 : associacao.hashCode());
+		result = prime * result + (ativo ? 1231 : 1237);
+		result = prime * result + ((criador == null) ? 0 : criador.hashCode());
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AssociacaoCriador other = (AssociacaoCriador) obj;
+		if (adm != other.adm)
+			return false;
+		if (associacao == null) {
+			if (other.associacao != null)
+				return false;
+		} else if (!associacao.equals(other.associacao))
+			return false;
+		if (ativo != other.ativo)
+			return false;
+		if (criador == null) {
+			if (other.criador != null)
+				return false;
+		} else if (!criador.equals(other.criador))
+			return false;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
 	
 }
