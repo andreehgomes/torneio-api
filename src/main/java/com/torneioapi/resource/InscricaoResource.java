1689eedc9ab9bceb097321b692bcaa0f8fd626f5
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-02 15:56:11 -0300

    Cria metodo buscaPeloCodigo() em InscricaoResource

--- src/main/java/com/torneioapi/resource/InscricaoResource.java
+++ src/main/java/com/torneioapi/resource/InscricaoResource.java
@@ -1,9 +1,12 @@
 package com.torneioapi.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,15 +14,22 @@ import com.torneioapi.model.Inscricao;
 import com.torneioapi.repository.InscricaoRepository;
 
 @RestController
-@RequestMapping("/incricoes")
+@RequestMapping("/inscricoes")
 public class InscricaoResource {
 
 	@Autowired
 	private InscricaoRepository inscricaoRepository;
-	
+
 	@GetMapping
-	public List<Inscricao> listar(){
+	public List<Inscricao> listar() {
 		return inscricaoRepository.findAll();
 	}
-	
+
+	@GetMapping("/{codigo}")
+	public ResponseEntity<Inscricao> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Inscricao> retorno = inscricaoRepository.findById(codigo);
+
+		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
+	}
+
 }
