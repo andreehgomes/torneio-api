6269f586f05f1701ad3ef6fa50cd8848ebdf36cd
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-02 16:23:57 -0300

    Cria método incluir em InscricaoResource

--- src/main/java/com/torneioapi/resource/InscricaoResource.java
+++ src/main/java/com/torneioapi/resource/InscricaoResource.java
@@ -3,13 +3,21 @@ package com.torneioapi.resource;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Inscricao;
 import com.torneioapi.repository.InscricaoRepository;
 
@@ -19,6 +27,9 @@ public class InscricaoResource {
 
 	@Autowired
 	private InscricaoRepository inscricaoRepository;
+	
+	@Autowired
+	private ApplicationEventPublisher publisher;
 
 	@GetMapping
 	public List<Inscricao> listar() {
@@ -32,4 +43,12 @@ public class InscricaoResource {
 		return !retorno.isEmpty() ? ResponseEntity.ok(retorno.get()) : ResponseEntity.notFound().build();
 	}
 
+	@PostMapping
+	public ResponseEntity<Inscricao> incluir(@Valid @RequestBody Inscricao inscricao, HttpServletResponse response){
+		Inscricao inscricaoSalva = inscricaoRepository.save(inscricao);
+		
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, inscricaoSalva.getId()));
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(inscricaoSalva);
+	}
 }
