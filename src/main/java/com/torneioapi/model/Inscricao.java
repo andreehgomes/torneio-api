9c5b894807488a1525051fd8821151f9c389b7e2
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-02 16:23:22 -0300

    Cria estratégia de incremento de ID

--- src/main/java/com/torneioapi/model/Inscricao.java
+++ src/main/java/com/torneioapi/model/Inscricao.java
@@ -3,6 +3,8 @@ package com.torneioapi.model;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -14,6 +16,7 @@ import javax.validation.constraints.NotNull;
 public class Inscricao {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private LocalDateTime data;
 	
