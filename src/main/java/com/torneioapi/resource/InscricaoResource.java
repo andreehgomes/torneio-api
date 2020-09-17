6be6864c33d56e87ba83a3ddbc840fdd372b0fcc
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-17 19:03:25 -0300

    Ajustes na InscricaoResource

--- src/main/java/com/torneioapi/resource/InscricaoResource.java
+++ src/main/java/com/torneioapi/resource/InscricaoResource.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +51,6 @@ public class InscricaoResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, inscricaoSalva.getId()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(inscricaoSalva);
-	}
+	}	
+	
 }
