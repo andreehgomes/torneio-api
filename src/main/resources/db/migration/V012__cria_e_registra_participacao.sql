1253d961387ac61f0050e899b5fb54578bd0fb74
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-18 13:14:05 -0300

    Cria migration para Participacao

---
+++ src/main/resources/db/migration/V012__cria_e_registra_participacao.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS participacao
+(
+   id BIGSERIAL PRIMARY KEY,
+   fk_id_inscricao BIGINT,
+   fk_id_etapa BIGINT,
+   fk_id_ave BIGINT,
+   marcacao VARCHAR,
+   pontos INT,
+   FOREIGN KEY (fk_id_inscricao) REFERENCES inscricao (id),
+   FOREIGN KEY (fk_id_etapa) REFERENCES etapa (id),
+   FOREIGN KEY (fk_id_ave) REFERENCES ave (id)
+);
+INSERT INTO participacao
+(
+   fk_id_inscricao,
+   fk_id_etapa,
+   fk_id_ave,
+)
+VALUES
+(
+   1,
+   1,
+   1,
+)
