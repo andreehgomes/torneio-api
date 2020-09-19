56f9a5882ac4073ad9ce8d9ca6a37f308fc94a97
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-19 01:31:15 -0300

    Cria ParticipacaoResource

---
+++ src/main/java/com/torneioapi/service/ParticipacaoService.java
@@ -0,0 +1,36 @@
+package com.torneioapi.service;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.BeanDefinitionDsl.BeanSupplierContext;
+import org.springframework.stereotype.Service;
+
+import com.torneioapi.model.Participacao;
+import com.torneioapi.repository.ParticipacaoRepository;
+
+@Service
+public class ParticipacaoService {
+	
+	@Autowired
+	private ParticipacaoRepository participacaoRepository;
+	
+	public Participacao atualizar (Long codigo, Participacao participacao) {
+		Participacao participacaoSalva = buscarParticipacaoPeloCodigo(codigo);
+		
+		BeanUtils.copyProperties(participacao, participacaoSalva);
+		return participacaoRepository.save(participacaoSalva);
+	}
+	
+	public Participacao buscarParticipacaoPeloCodigo(Long codigo) {
+		Participacao participacaoRetorno = participacaoRepository.findById(codigo).get();
+		
+		if(participacaoRetorno == null) {
+			throw new NoSuchElementException();
+		}
+		
+		return participacaoRetorno;
+	}
+
+}
