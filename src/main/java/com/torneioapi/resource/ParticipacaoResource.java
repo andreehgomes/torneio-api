6e2e860a8d9f67a6770f6170fb3dee71fd77b440
andreehgomes <andrefelipefeliciogomes@gmail.com>
2020-09-19 01:43:46 -0300

    Cria metodo atualizar em ParticipacaoResource

--- src/main/java/com/torneioapi/resource/ParticipacaoResource.java
+++ src/main/java/com/torneioapi/resource/ParticipacaoResource.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.torneioapi.event.RecursoCriadoEvent;
 import com.torneioapi.model.Participacao;
 import com.torneioapi.repository.ParticipacaoRepository;
+import com.torneioapi.service.ParticipacaoService;
 
 @RestController
 @RequestMapping("/participacoes")
@@ -27,6 +29,9 @@ public class ParticipacaoResource {
 	
 	@Autowired
 	private ParticipacaoRepository participacaoRepository;
+	
+	@Autowired
+	private ParticipacaoService participacaoService;
 	
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -50,6 +55,12 @@ public class ParticipacaoResource {
 		publisher.publishEvent(new RecursoCriadoEvent(this, response, participacaoSalva.getId()));
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(participacaoSalva);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Participacao> atualizar (@PathVariable Long codigo, @Valid @RequestBody Participacao participacao){
+		Participacao participacaoSalva = participacaoService.atualizar(codigo, participacao);
+		return ResponseEntity.status(HttpStatus.OK).body(participacaoSalva);
 	}
 
 }
